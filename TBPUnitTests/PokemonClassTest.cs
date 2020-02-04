1d2cfcdd0f1a697b93388a02645c69739889e470
Mélanie V <melanie.vaubien@hotmail.com>
2020-02-04 10:50:44 +0100

    Projet Test

---
+++ TBPUnitTests/PokemonClassTest.cs
@@ -0,0 +1,46 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeamBuilderPkmn;
+
+namespace TBPUnitTests
+{
+    [TestClass]
+    public class PokemonClassTest
+    {
+        private readonly Pokemon _pkm;
+
+        public PokemonClassTest()
+        {
+            _pkm = new Pokemon();
+        }
+
+        [TestMethod]
+        public void PokemonWithNoNameTest()
+        {
+            var result = _pkm.Name;
+            Assert.AreEqual(result, null);
+        }
+
+        [TestMethod]
+        public void PokemonWithNameTest()
+        {
+            var newPkm = new Pokemon("Carapuce");
+            var result = newPkm.Name;
+            Assert.AreEqual(result, "Carapuce");
+        }
+
+        [TestMethod]
+        public void PokemonWNoTypes()
+        {
+            var type1 = _pkm.Type1.Name;
+            var type2 = _pkm.Type2.Name;
+            Assert.AreEqual(type1, "none");
+            Assert.AreEqual(type2, "none");
+        }
+
+        [TestMethod]
+        public void GetTypeTest() {
+            var result = Type.GetType("water").Name;
+            Assert.AreEqual(result, "none");
+        }
+    }
+}
